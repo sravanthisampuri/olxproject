2d8399dd3553a82afd76e144d9d420436404fe32
venkatesh_g <venkatesh.g@mtwlabs.com>
2018-06-14 14:10:20 +0530

    changes

---
+++ src/app/properties/properties.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-properties',
+  templateUrl: './properties.component.html',
+  styleUrls: ['./properties.component.css']
+})
+export class PropertiesComponent implements OnInit {
+  classFocused:any;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  focused(){
+    this.classFocused = "active";
+  }
+
+}
