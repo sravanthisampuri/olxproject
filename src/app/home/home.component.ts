d6b9fb6c65d31fae84bb0e87ec1d55a1d32ba0bb
chanti123 <sukumar.p@mtwlabs.com>
2018-06-25 17:06:10 +0530

    location

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,28 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   data;
-  textfield;
-public locationSearch :boolean=false;
-  public url: any=["assets/images/olx.gif","assets/images/olx1.gif","assets/images/olx3.gif",
-  "assets/images/olx4.gif","assets/images/olx5.gif","assets/images/olx6.gif"];
-  public display:any=["assets/images/olx.gif"];
-  public i:any=1;
+  public textfield: any;
+  public locationSearch: boolean = false;
+  public url: any = ["assets/images/olx.gif", "assets/images/olx1.gif", "assets/images/olx3.gif",
+    "assets/images/olx4.gif", "assets/images/olx5.gif", "assets/images/olx6.gif"];
+  public display: any = ["assets/images/olx.gif"];
+  public i: any = 1;
   constructor() { }
- 
+
   ngOnInit() {
   }
-  myclick(){
-    this.i<this.url.length ? this.display=this.url[this.i] :  this.url[this.i=0];
-   this.i++;
+  myclick() {
+    this.i < this.url.length ? this.display = this.url[this.i] : this.url[this.i = 0];
+    this.i++;
   }
 
-  mynewclick(){
-    this.locationSearch=!this.locationSearch;
+  mynewclick() {
+    this.locationSearch = !this.locationSearch;
+   
   }
 
-  cities(select){
-    this.data=select.target.childNodes[0].data
-console.log(select.target.childNodes[0].data);
-   this.textfield =this.data
+  cities(select) {
+    this.data = select.target.childNodes[0].data
+    console.log(select.target.childNodes[0].data);
+    this.textfield = this.data;
+    this.mynewclick();
   }
- }
+}
