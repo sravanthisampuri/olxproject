dbf9e80fdc2ca1b5bbb1e2b2895dd77852d7864d
saiprakashgolla <saiprakash.g@mtwlabs.com>
2018-06-27 23:04:33 +0530

    myaccount

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,44 +5,53 @@ import { Router } from '@angular/router';
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
-  
+
 })
 export class LoginComponent implements OnInit {
-loginData={
-  password:'',
-  name:'',
-  email:''
-  
+  loginData = {
+    password: '',
+    name: '',
+    email: ''
 
-};
-public udata : any;
+
+  };
+  public udata: any;
 
   constructor(
-    private route : Router
+    private route: Router,
+    //private loginDetailsService: LoginDetailsService
+
   ) { }
 
   ngOnInit() {
   }
-loginUser(){
- // console.log(this.loginData);
-   this.udata = this.loginData;
-   console.log(this.udata.email);
-   if(!this.udata.email){
-    alert("Please Provide Email");
-  }else if(!this.udata.password){
-    alert("Please Provide Password");
+  loginUser() {
+    // console.log(this.loginData);
+    this.udata = this.loginData;
+    console.log(this.udata.email);
+    if (!this.udata.email) {
+      alert("Please Provide Email");
+    } else if (!this.udata.password) {
+      alert("Please Provide Password");
+    }
+
+    else {
+       if(this.udata.email == localStorage.getItem('email') && this.udata.password == localStorage.getItem('password'))
+      // this.loginDetailsService.submitAdd(this.loginData)
+      //   .subscribe(
+      //     function (response) {
+      //       console.log(response)
+      //     }
+      //   )
+
+       {
+         //this.route.navigate([''])
+       }
+    else    {
+  alert("invalid credentials")
+ }
   }
 
-   else{
-     if(this.udata.email == localStorage.getItem('email') && this.udata.password == localStorage.getItem('password'))
-   {
-    this.route.navigate([''])
-   }
-   else
-   {
-     alert("invalid credentials")
-   }
-  }
 
 
 }
