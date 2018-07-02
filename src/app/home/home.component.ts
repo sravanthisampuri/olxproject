2f3499326f6d8f795e497c1b723d2a8b59e61277
saiprakashgolla <saiprakash.g@mtwlabs.com>
2018-07-02 11:50:55 +0530

    changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  data;
+  public data;
   public citiesdata: any =["Ahmedabad","Bengaluru","Chandigarh","Chennai","Coimbatore","Delhi",
   "Gurgaon","Hyderabad","Jaipur","Kochi","Kolkata","Lucknow","Ludhiana","Mumbai","Nagpur","Pune","Surat",
    "Thiruvananthapuram"];
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -55,7 +55,7 @@ public profile:any;
           }
         )
      
-    this.route.navigate([''])
+    this.route.navigate(['myaccount'])
    }
    
   }
